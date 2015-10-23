900f58b51cf98f3e38ee591743b0b979aa0c3c39
pawoln <pawoln@gmail.com>
2015-10-23 23:50:03 +0400

    Blank functions for articles.php

---
+++ PHP/proj1/models/articles.php
@@ -0,0 +1,17 @@
+<?
+	function_all() {
+		
+	}
+	function_get($id) {
+		
+	}
+	function_new($title, $state, $content) {
+		
+	}
+	function_edit($id, $title, $date, $content) {
+		
+	}
+	function_delete($id) {
+		
+	}
+?>
