bcc6d4d643f65e14fa7580f56abecbdaff82bc62
pawoln <pawoln@gmail.com>
2015-11-20 15:38:21 +0400

    Edit function articles_all and articles_get

--- PHP/proj1/models/articles.php
+++ PHP/proj1/models/articles.php
@@ -1,10 +1,28 @@
 <?php
-	function articles_all(){
+	function articles_all($link){
+        $query="SELECT * FROM articles ORDER BY id DESC";
+            $result=mysqli_query($link,$query);
+      if  (!$result)
+          die(mysqli_error($link));
+        
+        $n=mysqli_num_rows($result);
+        $articles= array();
+        
+        for($i=0;$i<$n;$i++){
+            $row=mysqli_fetch_assoc($result);
+            $articles[]=$row;            
+        }
+        return $articles;
 		
 	}
 
-	function articles_get($id){
-		
+	function articles_get($link,$id_article){
+        $query=sprintf("SELECT * FROM articles WHERE id=%d",(int)$id_article);
+        $result=mysqli_query($link,$query);
+        if (!$result)
+            die(mysqli_error($link));
+		$article=mysqli_fetch_assoc($result);
+        return $article;
 	}
 
 	function articles_new($title, $state, $content){
