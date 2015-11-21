3c31daf649356ca1ab9d89ccb45b3e64cfbde481
pawoln <pawoln@gmail.com>
2015-11-21 16:24:41 +0400

    Add link for admin panel and edit path for css files

--- PHP/proj1/views/articles.php
+++ PHP/proj1/views/articles.php
@@ -3,13 +3,14 @@
     <head>
         <meta charset="utf8">
         <title>Мой первый блог</title>
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="../style.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
               
     </head>
     <body>
         <div class="container">
             <h1>Мой первый блог</h1>
+            <a href="admin">Панель Администратора</a>
             <div>
                 <?php foreach($articles as $a):?>
             <div class="article">
