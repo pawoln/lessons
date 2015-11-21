bb35932ad3446031cecc5c2c12221c2164c88c9a
pawoln <pawoln@gmail.com>
2015-11-21 16:23:14 +0400

    Edit path for views and models

--- PHP/proj1/index.php
+++ PHP/proj1/index.php
@@ -1,9 +1,9 @@
 <?php
-    require_once("database.php");
-	require_once("models/articles.php");
+    require_once("../database.php");
+	require_once("../models/articles.php");
 	
     $link=db_connect();
 	$articles=articles_all($link);
 
-    include("views/articles.php");
+    include("../views/articles.php");
 ?>
